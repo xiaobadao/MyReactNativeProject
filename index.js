07e165792bb19ef7eb1b1477469191ec83699479
xiaobadao <675499468@qq.com>
2017-12-25 11:16:21 +0800

    MyReactNativeProject
    只能用于真机测试。不支持模拟器。

---
+++ index.js
@@ -0,0 +1,8 @@
+
+import {AppRegistry} from 'react-native';
+
+import  SectionListBasics from './APP';
+AppRegistry.registerComponent('MyReactNativeApp', () => SectionListBasics);
+
+
+
---
+++ ios/MYRN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYRN
+//
+//  Created by apple on 2017/12/22.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
